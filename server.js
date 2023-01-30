5820c160f0853c117fd70dd66aed24b4b59bb3f9
Vanessa <vanessa.y.cui@gmail.com>
2023-01-30 10:33:42 -0500

    init

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+
+require('dotenv').config()
+require('./config/database');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Configure to use port 3001 instead of 3000 during
+// development to avoid collision with React's dev server
+const port = process.env.PORT || 3001;
+
+//listen for incoming requests:
+app.listen(port, function() {
+  console.log(`Express app running on port ${port}`)
+});
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+//catch-all route to return the idex.html on non-AJAX request
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
